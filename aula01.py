6fc0e5a599bd97d0559ae0b15fa491cb833eaa85
Gustavo <gussouza0088@gmail.com>
2024-10-23 17:24:27 -0300

    codigo para calculo do bonus

--- aula01.py
+++ aula01.py
@@ -1,6 +1,3 @@
-# calcular o valor do bonus de um funcionario, baseando-se no seu nome, salario e porcentagem de bonus
-# o calculo deve ser feito de acordo com as seguintes regras: 1000 + salario * bonus
-# o nome do funcionario e um string, o salario um float, e o bonus um float e todos os campos devem ser informados pelo usuario
 def calcular_bonus(nome, salario, bonus):
     return 1000 + salario * bonus
 
